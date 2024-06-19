55a601d38b102ba97a5f84091465759246cd23bf
Sergio Ramirez <hellosergior@proton.me>
2024-06-19 00:35:05 -0600

    anade el archivo para autenticacion

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -1,7 +1,5 @@
 package controller;
 
-import java.security.Provider.Service;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
